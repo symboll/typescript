a87681edbd979c3b8ded4101ef2df936bbce7a78
likanghua <lkh19890913@163.com>
2020-09-23 22:04:59 +0800

    feat: add basic-type

--- src/example/basic_type.ts
+++ src/example/basic_type.ts
@@ -1,7 +1,88 @@
-// 数字 类型
-let n: number = 12
-// n = 'ab'
-console.log(n)
+// boolean
+let bool:boolean = false
+bool = true
+// bool = 'abc'
 
+// number
+let num: number = 123
+// num = 'abc'
+num = 0b1111011
+num = 0o173
+num = 0x7b
+
+// string
 let str: string = 'abc'
-console.log(str)
+str = `数值是${num}`
+
+// Array
+let arr1: number[] = [1, 2, 3]
+let arr2: Array<string> = ['a', 'b']
+let arr3: (number| string)[] = [1,2,3,'a']   // 联合类型
+let arr4: Array<number| string> = [1,2,3,'a']
+
+// tuple
+let tuple:[number, string] = [ 1,'s']
+
+// enum
+enum Roles {
+  USER,
+  ADMIN = 4,
+  SUPET_ADMIN
+}
+console.log(Roles[4])
+
+// any
+let value: any = 123
+value = 'abc'
+value = false
+let arr5: any[] = [1,2,'a', false]
+
+// void
+let res = (function (arg: string):void {})('')
+res = undefined
+// res = null
+
+// undefined & null
+let u: undefined = undefined
+let n: null = null
+
+// never
+// 1. 抛错
+// 2. 死循环
+let errorfunc = function() {
+  throw new Error()
+}
+let infinitefunc = function() {
+  while(true){}
+}
+// let a = errorfunc()
+// u = a
+// n = a
+
+// Object
+let obj = { name: 'symbollee' }
+function printObj(obj: object):void {
+  console.log(obj)
+}
+printObj(obj)
+// printObj(new Date())
+// printObj([1,2,3])
+
+
+// 类型断言
+const getLength = (target: number| string):number => {
+  if((target as string).length || (<string>target).length === 0) {
+    return (target as string).length
+  }else {
+    return target.toString().length
+  }
+}
+
+// 或者使用自定义类型保护
+const getLength2 = (target: number| string):number => {
+  if(typeof target === 'string') {
+    return target.length
+  }else {
+    return target.toString().length
+  }
+}
