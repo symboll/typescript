51b2f0557e3f047e37fb16b1145eabc5e0c60c59
likanghua <lkh19890913@163.com>
2020-09-25 23:50:24 +0800

    feat: modified symbol

--- src/example/symbol.ts
+++ src/example/symbol.ts
@@ -11,7 +11,7 @@ let s5: symbol = Symbol(123)
 
 // symbol 值 不能与其他类型的值 做运算
 // symbol 值可以转换成string | boolean
-let s6: symbol = Symbol()
+const s6: unique symbol = Symbol()
 // s6.toString()
 // Boolean(s6)
 
@@ -25,7 +25,7 @@ const info = {
 // console.log(Object.getOwnPropertyNames(info))     // ["name"]
 // console.log(Object.getOwnPropertySymbols(info))   // [Symbol()]
 // console.log(Reflect.ownKeys(info))                 // ["name", Symbol()]
-// info[s6]        // 类型“symbol”不能作为索引类型使用。ts(2538)
+console.log(info[s6])
 
 // symbol 的2个静态方法
 let s7 = Symbol.for("zhangsan")
@@ -34,6 +34,10 @@ let s8 = Symbol.for("zhangsan")
 // Symbol.keyFor(s7)    // 'zhangsan'
 
 class A {
+  public static [Symbol.hasInstance](value: any) {
+    console.log("==>", value)
+    return value instanceof A;
+  }
   constructor(private name: string) {}
   public [s6]() {
     return this.name
@@ -44,7 +48,9 @@ class A {
 }
 
 const ab = new A("nativeLee")
-// console.log(ab[s6]())         //"symbol" 的表达式不能用于索引类型
+// console.log(ab[s6]())
 // console.log(ab.getName())     // ok
 
+// console.log(ab instanceof A)
 // 11个内置symbol值
+// Symbol.hasInstance
